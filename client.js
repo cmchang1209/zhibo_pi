14cb2b84fa8571de4cd3678433dd20134d59554a
cmchang <cmchang120956@gmail.com>
2021-04-16 16:13:35 +0800

    add install pi douctiom

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ const maxPort = 60000
 const awsPort = 8090
 const awsHost = 'videostream.fidodarts.com'
 const url = `http://${awsHost}:${awsPort}`
+const FFmpegWsd = 'fidodarts'
 //const url = `http://192.168.2.130:${awsPort}`
 
 let myMac = ''
@@ -26,7 +27,7 @@ ioClient.on('connect', () => {
     console.log('connect')
     run()
     intervalId = setInterval(() => {
-        run()
+        //run()
     }, 10000)
 })
 
@@ -34,16 +35,19 @@ ioClient.on("disconnect", () => {
     clearInterval(intervalId)
 })
 
-ioClient.on('startFFmpeg', (data) => {
-    var cmd = `/home/pi/ffmpeg.sh '${data.cmd}'`
+ioClient.on('runFFmpeg', (data) => {
+    var cmd = `lsof -i:${data.port} -t`
     exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error('An error occurred: ', error)
+        var pid = stdout * 1
+        if (!pid) {
+            //var cmd = `ffmpeg ${data.cmd}`
+            //runFFmpeg(cmd, data)
+            runFFmpeg(data)
         } else {
-            console.log('stdout:', stdout)
-            console.log('stderr:', stderr)
+            ioClient.emit('echoPlayImage', { me: data.me, data: { status: true }, indexSid: data.indexSid })
         }
     })
+
 })
 
 ioClient.on('fcnr', (data) => {
@@ -60,6 +64,41 @@ async function fcnr(cmd, data) {
     }
 }
 
+async function runFFmpeg(data) {
+    console.log(data)
+    var size = data.imageInfo[0].size
+    if (data.imageInfo[0].type === 1) {
+        size = '640x360'
+    }
+    var ws_realy_url = `http://${awsHost}:${data.port}/${FFmpegWsd}`
+    var cmd = `/home/pi/ffmpeg.sh ${data.imageInfo[0].fps} ${size} '${data.imageInfo[0].dev_name}' '${ws_realy_url}'`
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(error)
+            //ffmpeg 未啟動
+            ioClient.emit('echoPlayImage', { me: data.me, data: { status: false, code: '0004' }, indexSid: data.indexSid })
+        } else {
+            console.log('ok')
+            ioClient.emit('echoPlayImage', { me: data.me, data: { status: true, port: data.port }, indexSid: data.indexSid })
+        }
+    })
+    return
+    var d = await exec(cmd)
+    setTimeout(() => {
+        cmd = `lsof -i:${data.port} -t`
+        exec(cmd, (error, stdout, stderr) => {
+            console.log(stdout * 1, error)
+            var pid = stdout * 1
+            if (pid) {
+                ioClient.emit('echoPlayImage', { me: data.me, data: { status: true, port: data.port }, indexSid: data.indexSid })
+            } else {
+                //ffmpeg 未啟動
+                ioClient.emit('echoPlayImage', { me: data.me, data: { status: false, code: '0004' }, indexSid: data.indexSid })
+            }
+        })
+    }, 1000)
+}
+
 async function run() {
     var data = []
     var cmd = "v4l2-ctl --list-devices"
@@ -67,12 +106,12 @@ async function run() {
         var usbList = listUSB(stdout)
         usbList.map((item, index) => {
             item.devices.map((uitem, uindex) => {
-                if(uindex === 0) {
+                if (uindex === 0) {
                     var cmd1 = `v4l2-ctl --list-formats-ext -d ${uitem.name}`
-                    exec(cmd1, (error,stdout, stderr) => {
+                    exec(cmd1, (error, stdout, stderr) => {
                         var formats = formatExt(stdout)
                         uitem.formats.push(formats)
-                        if(index === usbList.length -1) {
+                        if (index === usbList.length - 1) {
                             ioClient.emit('run', { mac: myMac, camData: usbList })
                         }
                     })
